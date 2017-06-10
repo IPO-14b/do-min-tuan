7e55888f74bd0f65a4ac0ead536f4ef9a17475c5
Se1ko <kisame453@gmail.com>
2017-06-10 21:26:59 +0300

    Adding docblock to functions

--- browser_button.js
+++ browser_button.js
@@ -1,40 +1,54 @@
-﻿kangoLoader.add("browser_button", function(require, exports, module) {
-	function BrowserButtonBase(t) {
-		EventTarget.call(this), this._details = t
+﻿/*
+ * Загрузка модуля
+ */
+kangoLoader.add("browser_button", function(require, exports, module) {
+    function BrowserButtonBase(t) {
+        EventTarget.call(this), this._details = t
 	}
 
+/*
+ * Создание кнопки расширения, которая отображается на панели расширений браузера
+ * 
+ * @_onClicked обрабатывает события при нажатии на иконку
+ * @_initDetails инициализация свойств
+ * @setTooltipText задает тултип
+ * @setCaption задает тултип
+ * @setIcon задает иконку
+ * @setBadgeValue задает значек
+ * @setBadgeBackgroundColor задает цвет бекграунда
+ */
 function BrowserButton(t) {
     BrowserButtonBase.call(this, t), this._popupDetails = null, chrome.browserAction.onClicked.addListener(func.bind(this._onClicked, this)), this._initDetails(t)
-	}
-	var Info = require("info");
-	BrowserButton.prototype = object.extend(BrowserButtonBase, {
-		_onClicked: function() {
-			return this.fireEvent(this.event.COMMAND)
-		},
-		_initDetails: function(t) {
-			object.isObject(t) && (object.isString(t.icon) && this.setIcon(t.icon), object.isString(t.caption) && this.setCaption(t.caption))
-		},
-		setTooltipText: function(t) {
-			chrome.browserAction.setTitle({
-				title: t.toString()
-			})
-		},
-		setCaption: function(t) {},
-		setIcon: function(t) {
-			chrome.browserAction.setIcon({
-				path: io.getFileUrl(t)
-			})
-		},
-		setBadgeValue: function(t) {
-			chrome.browserAction.setBadgeText({
-				text: null != t && 0 != t ? t.toString() : ""
-			})
-		},
-		setBadgeBackgroundColor: function(t) {
-			chrome.browserAction.setBadgeBackgroundColor({
-				color: t
-			})
-		},
-	});
+    }
+    var Info = require("info");
+    BrowserButton.prototype = object.extend(BrowserButtonBase, {
+        _onClicked: function() {
+            return this.fireEvent(this.event.COMMAND)
+        },
+        _initDetails: function(t) {
+            object.isObject(t) && (object.isString(t.icon) && this.setIcon(t.icon), object.isString(t.caption) && this.setCaption(t.caption))
+        },
+        setTooltipText: function(t) {
+            chrome.browserAction.setTitle({
+            title: t.toString()
+            })
+        },
+        setCaption: function(t) {}, //todo
+        setIcon: function(t) {
+            chrome.browserAction.setIcon({
+                path: io.getFileUrl(t)
+            })
+        },
+        setBadgeValue: function(t) {
+           chrome.browserAction.setBadgeText({
+                text: null != t && 0 != t ? t.toString() : ""
+            })
+        },
+        setBadgeBackgroundColor: function(t) {
+            chrome.browserAction.setBadgeBackgroundColor({
+                color: t
+            })
+        },
+    });
 
 });
