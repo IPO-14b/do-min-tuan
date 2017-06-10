46116583b13861d7b7332dab55584de6e55c7d59
Se1ko <kisame453@gmail.com>
2017-06-10 20:48:21 +0300

    commit

--- api.js
+++ api.js
@@ -22,17 +22,6 @@
 		}
 	}
 
-	function wrapMessageTarget(e) {
-		function n() {
-			array.forEach(r, function(n) {
-				e.removeMessageListener(n.name, n.listener)
-			}), r = []
-		}
-		var r = [];
-		return e.addEventListener = func.decorate(e.addEventListener, function(e, n) {
-			var t = n[0],
-				o = n[1];
-			return e.call(this, t, o);
-	}
+	
 
 });
