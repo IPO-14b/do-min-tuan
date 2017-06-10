463a93303aa91a88a58bdc33ad9e0b14f6f41581
Se1ko <kisame453@gmail.com>
2017-06-10 19:55:46 +0300

    adding info var to browser button

--- browser_button.js
+++ browser_button.js
@@ -3,4 +3,38 @@
 		EventTarget.call(this), this._details = t
 	}
 
+function BrowserButton(t) {
+    BrowserButtonBase.call(this, t), this._popupDetails = null, chrome.browserAction.onClicked.addListener(func.bind(this._onClicked, this)), this._initDetails(t)
+	}
+	var Info = require("info");
+	BrowserButton.prototype = object.extend(BrowserButtonBase, {
+		_onClicked: function() {
+			return this.fireEvent(this.event.COMMAND)
+		},
+		_initDetails: function(t) {
+			object.isObject(t) && (object.isString(t.icon) && this.setIcon(t.icon), object.isString(t.caption) && this.setCaption(t.caption))
+		},
+		setTooltipText: function(t) {
+			chrome.browserAction.setTitle({
+				title: t.toString()
+			})
+		},
+		setCaption: function(t) {},
+		setIcon: function(t) {
+			chrome.browserAction.setIcon({
+				path: io.getFileUrl(t)
+			})
+		},
+		setBadgeValue: function(t) {
+			chrome.browserAction.setBadgeText({
+				text: null != t && 0 != t ? t.toString() : ""
+			})
+		},
+		setBadgeBackgroundColor: function(t) {
+			chrome.browserAction.setBadgeBackgroundColor({
+				color: t
+			})
+		},
+	});
+
 });
