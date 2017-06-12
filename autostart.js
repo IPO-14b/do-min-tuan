ef069cf6fe9b909f4f3b8feceb5e1a85f4de8883
Se1ko <kisame453@gmail.com>
2017-06-12 09:35:22 +0300

    Fix syntax for if clause and add semicolon

--- autostart.js
+++ autostart.js
@@ -2,14 +2,14 @@
 
     var is_lingapp = false;
     if (document.domain == "vocabla.com" || document.domain == "staging.vocabla.com") {
-        is_lingapp = true
+        is_lingapp = true;
     };
 
     if (window.location.protocol == "http:" || window.location.protocol == "https:") {
         chrome.extension.sendRequest({
             operation: "hostname",
             value: document.domain
-        })
+        });
 
         chrome.extension.sendRequest({
             key: 'status',
@@ -18,14 +18,14 @@
             if (res.val == "on" || is_lingapp) {
                 setLingolet();
             }
-        })
+        });
 
         function setLingolet() {
             if (!document.getElementById('script')) {
-                var x = document.createElement('script')
-                x.setAttribute('id', 'script')
-                x.src = chrome.extension.getURL("page_script.js")
-                document.body.appendChild(x)
+                var x = document.createElement('script');
+                x.setAttribute('id', 'script');
+                x.src = chrome.extension.getURL("page_script.js");
+                document.body.appendChild(x);
             }
         }
     }
